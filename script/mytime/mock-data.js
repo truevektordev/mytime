4b6c46f256eaa1e7d09764a77c0b5b19c2993cb5
david.wolverton@vodori.com <david.wolverton@vodori.com>
2014-05-20 09:36:39 -0400

    Mock data for quick testing

--- script/mytime/mock-data.js
+++ script/mytime/mock-data.js
@@ -1,10 +1,29 @@
 define([
-    'mytime/command/CreateTaskCommand'
-], function(CreateTaskCommand) {
+    "mytime/command/CreateTaskCommand", "mytime/command/CreateTimeEntryCommand",
+    "mytime/util/DateTimeUtil"
+], function(CreateTaskCommand, CreateTimeEntryCommand,
+            DateTimeUtil) {
     return function() {
-        new CreateTaskCommand({ task: { code: 'CAYENNE-1234', name: 'Squash a Bug' } }).exec();
-        new CreateTaskCommand({ task: { code: 'CAYENNE-456', name: 'Make it Work' } }).exec();
-        new CreateTaskCommand({ task: { code: 'CAYENNE-789', name: 'Do Something Awesome' } }).exec();
-        new CreateTaskCommand({ task: { code: 'PSP-100', name: 'Uh Oh...' } }).exec();
+        new CreateTaskCommand({ task: { code: "CAYENNE-1234", name: "Squash a Bug" } }).exec();
+        new CreateTaskCommand({ task: { code: "CAYENNE-456", name: "Make it Work" } }).exec();
+        new CreateTaskCommand({ task: { code: "CAYENNE-789", name: "Do Something Awesome" } }).exec();
+        new CreateTaskCommand({ task: { code: "PSP-100", name: "Uh Oh..." } }).exec();
+
+        new CreateTimeEntryCommand({ timeEntry: {
+            date: DateTimeUtil.getCurrentDate(), startHour: 8.5, endHour: 9.75,
+            taskId: 2, text: "It works!"
+        }}).exec();
+        new CreateTimeEntryCommand({ timeEntry: {
+            date: DateTimeUtil.getCurrentDate(), startHour: 9.75, endHour: 10.5,
+            taskId: 1, text: "Squashing Bugs"
+        }}).exec();
+        new CreateTimeEntryCommand({ timeEntry: {
+            date: DateTimeUtil.getCurrentDate(), startHour: 10.5, endHour: 10.75,
+            text: "Daily Scrum"
+        }}).exec();
+        new CreateTimeEntryCommand({ timeEntry: {
+            date: DateTimeUtil.getCurrentDate(), startHour: 10.75, endHour: 12,
+            taskId: 1, text: "Squishing Bugs... Particularly this nasty one with bright colors."
+        }}).exec();
     };
 });
