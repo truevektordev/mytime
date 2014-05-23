2c8625fb25e31946e35bd88fe73f5197fc51e0b9
david.wolverton@vodori.com <david.wolverton@vodori.com>
2014-05-23 00:53:37 -0400

    DaysInWeekList displays actual times

--- script/mytime/widget/DaysInWeekList.js
+++ script/mytime/widget/DaysInWeekList.js
@@ -2,9 +2,9 @@ define([
     "dojo/_base/declare",
     "lodash",
     "dojo/_base/lang",
-    "dojo/string", "dojo/on", "dojo/query",
+    "dojo/number", "dojo/on", "dojo/query", "dojo/when",
     "dojo/dom-construct", "dojo/dom-class", "dojo/dom-style", "dojo/dom-geometry",
-    "dojo/Evented", "dojo/store/Observable", "dojo/date", "dojo/date/locale",
+    "dojo/Evented", "dojo/date", "dojo/date/locale",
     "dijit/_WidgetBase", "dijit/_TemplatedMixin",
     "mytime/util/DateTimeUtil",
     "dojo/text!mytime/widget/DaysInWeekList/template.html"
@@ -12,9 +12,9 @@ define([
 function (declare,
     _,
     lang,
-    stringUtil, on, query,
+    number, on, query, when,
     domConstruct, domClass, domStyle, domGeometry,
-    Evented, Observable, dojoDate, dateLocale,
+    Evented, dojoDate, dateLocale,
     _WidgetBase, _TemplatedMixin,
     DateTimeUtil,
     template) {
@@ -28,8 +28,18 @@ function (declare,
      */
     return declare([_WidgetBase, _TemplatedMixin, Evented], {
         templateString: template,
+
         selectedDate: DateTimeUtil.getCurrentDate(),
+
+        timeEntryStore: null,
+
+        /**
+         * a JavaScript Date
+         */
         _firstDayOfWeek: null,
+        /**
+         * an integer 0 to 6
+         */
         _selectedDayOfWeek: null,
 
         _headerNode: null,
@@ -46,6 +56,7 @@ function (declare,
             this._selectedDayOfWeek = date.getDay();
             this._firstDayOfWeek = dojoDate.add(date, 'day', -this._selectedDayOfWeek);
             this._fillIn();
+            this._updateHours();
         },
 
         buildRendering: function() {
@@ -61,6 +72,7 @@ function (declare,
                 this._dayHoursNodes[i] = row.cells[2];
             }
             this._fillIn();
+            this._updateHours();
         },
 
         _fillIn: function() {
@@ -87,21 +99,80 @@ function (declare,
 
         postCreate: function() {
             var _this = this;
-            on(this._containerNode, on.selector('tr', 'click'), function() {
-                var row = this;
-                for (var i = 0; i < 7; i++) {
-                    if (row === _this._dayRowNodes[i]) {
-                        var dateToSelect = DateTimeUtil.convertDateObjectToDateString(_this._getDateOfNthDay(i));
-                        if (dateToSelect !== _this.selectedDate) {
-                            _this.set('selectedDate', dateToSelect);
+            this.own(
+                on(this._containerNode, on.selector('tr', 'click'), function() {
+                    var row = this;
+                    for (var i = 0; i < 7; i++) {
+                        if (row === _this._dayRowNodes[i]) {
+                            var dateToSelect = DateTimeUtil.convertDateObjectToDateString(_this._getDateOfNthDay(i));
+                            if (dateToSelect !== _this.selectedDate) {
+                                _this.set('selectedDate', dateToSelect);
+                            }
                         }
                     }
-                }
-            });
+                }),
+                this.watch('timeEntryStore', lang.hitch(this, '_updateHours'))
+            )
         },
 
         _getDateOfNthDay: function(n) {
             return dojoDate.add(this._firstDayOfWeek, 'day', n);
+        },
+
+        _updateHours: function() {
+            if (!this.timeEntryStore || !this._headerNode || !this._firstDayOfWeek) {
+                return; // too early to render
+            }
+            when(this.timeEntryStore.query({
+                "date>=": DateTimeUtil.convertDateObjectToDateString(this._firstDayOfWeek),
+                "date<=": DateTimeUtil.convertDateObjectToDateString(this._getDateOfNthDay(6))
+            }), lang.hitch(this, '_renderHours'));
+        },
+
+        _renderHours: function(timeEntries) {
+            var total = 0;
+            var dayTotal = {};
+            for (var i = 0; i < 7; i++) {
+                dayTotal[DateTimeUtil.convertDateObjectToDateString(this._getDateOfNthDay(i))] = 0;
+            }
+
+            _.forEach(timeEntries, function(timeEntry) {
+                var hours = timeEntry.endHour - timeEntry.startHour;
+                total += hours;
+                dayTotal[timeEntry.date] += hours;
+            });
+
+            for (var i = 0; i < 7; i++) {
+                var hours = dayTotal[DateTimeUtil.convertDateObjectToDateString(this._getDateOfNthDay(i))];
+                this._dayHoursNodes[i].innerHTML = this._formatHours(hours);
+                domClass.toggle(this._dayHoursNodes[i], "zero-time", hours === 0);
+            }
+            this._weekTotalNode.innerHTML = this._formatHours(total);
+            domClass.toggle(this._weekTotalNode, "zero-time", total === 0);
+
+            this._renderDifference(total);
+        },
+
+        _renderDifference: function(total) {
+            var daysIntoWeek = dojoDate.difference(this._firstDayOfWeek);
+            if (daysIntoWeek > 5) {
+                daysIntoWeek = 5;
+            }
+            var diff = 0;
+            if (daysIntoWeek >= 1) {
+                diff = total - (daysIntoWeek * 8);
+            }
+
+            var diffString = this._formatHours(Math.abs(diff));
+            var diffSign = diff < 0 ? "- " : (diff > 0 ? "+ " : "")
+
+            this._weekDiffNode.innerHTML = diffSign + diffString;
+            domClass.toggle(this._weekDiffNode, "in-the-black", diff >= 0);
+            domClass.toggle(this._weekDiffNode, "in-the-red", diff < 0);
+        },
+
+        _formatHours: function(hour) {
+            return number.format(hour, { places: 2}) + '<span>h</span>';
         }
     });
 });
